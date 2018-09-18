7eaf954f916a7dad159170bccd02b56746900613
Sachin Jaishankar <sachin.jaishankar@duke.edu>
2018-09-17 20:20:21 -0400

    final

--- src/NBody.java
+++ src/NBody.java
@@ -63,8 +63,8 @@ public class NBody {
 	}
 	
 	public static void main(String[] args) throws FileNotFoundException{
-		double totalTime = 157788000.0;
-		double dt = 25000.0;
+		double totalTime = 1000000000.0;
+		double dt = 1000000.0;
 		
 		String fname= "./data/planets.txt";
 		if (args.length > 2) {
